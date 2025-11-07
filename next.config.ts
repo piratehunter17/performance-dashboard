049e72c22c97b07ccf5cad4d7e5fe85a19915994
Nitish krishna G <cb.en.u4cse22428@cb.students.amrita.edu>
2025-11-07 20:44:30 +0530

    final

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  
+  // Add this redirects function
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/dashboard',
+        permanent: true, // Use true for production
+      },
+    ]
+  },
 };
 
 export default nextConfig;
